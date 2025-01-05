2bc8c9749c122a37ff7330862f834fe39ee7e87c
RACollins <richardcollins128@gmail.com>
2025-01-05 23:48:24 +0900

    Refactor landing page and add custom styles

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fasthtml.common import *
 from pages.landing import LandingPage
 
-app, rt = fast_app()
+### Set head elements
+css_styles = Link(rel="stylesheet", href="/css/styles.css", type="text/css")
+
+### Set up FastHTML app
+app, rt = fast_app(live=True, hdrs=[css_styles])
 
 
 ### Set up routes
 @rt("/")
 def get():
-    return (Title("Habit Slap"), Container(LandingPage()))
+    return Title("Habit Slap"), Container(LandingPage())
 
 
 serve()
